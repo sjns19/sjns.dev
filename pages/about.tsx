5f974963ef2adffa13eb2f713bf3f1a8459c2ecc
Sujan Shrestha <sujan@Sujans-MacBook-Pro.local>
2024-01-06 14:53:16 +0545

    og fixes

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,7 @@ const About: NextPage = () => {
 			<Meta
 				title="About - Sujan Shrestha"
 				description="Hello there! My name is Sujan Shrestha. As a self-taught person, I started coding in 2013 when I decided to create a community for a game's multiplayer server. This initial experience opened the doors for me to enter the world of programming. In 2016, I shifted my focus towards web development, which has now become my main area of interest."
-				ogImage={`${SITE_URL}/me.png`}
+				ogImage={`${SITE_URL}/og-banner.png`}
 				ogURL={`${SITE_URL}/about`}
 			/>
 			<AboutPage />
