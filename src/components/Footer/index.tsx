68127fd279ecbbe192e213905c356ac2bd8bd3a2
Sujan Shrestha <sujan@Sujans-MacBook-Pro.local>
2024-03-22 13:34:37 +0545

    Changes to the link text color throughout different themes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,12 +1,22 @@
 import type FooterLinkProps from './types';
 import classes from './Footer.module.scss';
-import { Text, Container, Link } from '@/components';
+import { Text, Container, Pad, Link, IconFrame } from '@/components';
+import { FiGithub } from 'react-icons/fi';
 
 export default function Footer() {
 	return (
 		<Container element="footer">
 			<div className={classes['footer']}>
-				<Text element="h3" color="dark-grey">sjns.dev</Text>
+				<Text element="h3" color="dark-grey">
+					sjns.dev
+					<Pad element="span">
+						<Link href="https://github.com/sjns19/sjns.dev" title="sjns.dev repository">
+							<IconFrame>
+								<FiGithub size={16} />
+							</IconFrame>
+						</Link>
+					</Pad>
+				</Text>
 				<Text size="sm" color="dark-grey">
 					Designing is hard. Improvising, not so much... Crafted using <FooterLink link="https://nextjs.org/">Next.js</FooterLink> w/ <FooterLink link="https://www.typescriptlang.org/">TypeScript</FooterLink>, <FooterLink link="https://docs.pmnd.rs/zustand/getting-started/introduction">Zustand</FooterLink> && <FooterLink link="https://sass-lang.com/">SASS</FooterLink> by me.
 				</Text>
